811f48ce46768170b43345cb16c3cccc2d1f3c98
Aboubacar04 <aboubacrisow99@gmail.com>
2025-09-23 15:05:57 +0000

    Préparer le projet pour mise à jour Angular 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponentComponent } from "./layout/header-component/header-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
